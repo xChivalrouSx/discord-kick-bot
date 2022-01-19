f3f0ae7189a653af0915ecf16d346b013e4d51ca
Mert Çakar <mertcakar.es@gmail.com>
2022-01-20 02:53:52 +0300

    Some print log is added.

--- src/kick-bot-main.py
+++ src/kick-bot-main.py
@@ -21,14 +21,19 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
 	if (after.channel != None) and (after.channel.name == KICK_CHANNEL):
+		print("Change detected on kick channel...")
+
 		guild = discord.utils.get(client.guilds, name=SERVER_NAME)
 		channel = discord.utils.get(guild.channels, name=MESSAGE_CHANNEL)
 		
 		if (not member.guild_permissions.administrator) and (not member.guild_permissions.ban_members):
-			kick_message = member.name + " kicked!.. \n" + KICK_MESSAGE
+			print("Member will be kicked soon...")
 
+			kick_message = member.name + " kicked!.. \n" + KICK_MESSAGE
 			await member.send(kick_message + "\n" + KICK_PRIVATE_MESSAGE)
 			await channel.send(kick_message)
 			await member.kick()
+
+			print("Member kicked...")
 
 client.run(TOKEN)
