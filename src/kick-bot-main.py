1f3c82abfde43211b329db0f6d762f96b7ca78b4
Mert Çakar <mertcakar.es@gmail.com>
2022-01-23 22:53:04 +0300

    Daily message send is added.

--- src/kick-bot-main.py
+++ src/kick-bot-main.py
@@ -1,7 +1,9 @@
 import os
 
 import discord
-from discord.ext import commands
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,18 +14,36 @@ KICK_CHANNEL = os.getenv('KICK_VOICE_CHANNEL')
 KICK_MESSAGE = os.getenv('KICK_MESSAGE')
 KICK_PRIVATE_MESSAGE = os.getenv('KICK_PRIVATE_MESSAGE')
 
-client = commands.Bot(command_prefix = '.')
+DAILY_MESSAGE_TIME = os.getenv('DAILY_MESSAGE_TIME')
+DAILY_MESSAGE_CHANNEL = os.getenv('DAILY_MESSAGE_CHANNEL')
+DAILY_MESSAGE = os.getenv('DAILY_MESSAGE')
 
-@client.event
+bot = commands.Bot(command_prefix = '.')
+
+async def func():
+	await bot.wait_until_ready()
+	guild = discord.utils.get(bot.guilds, name=SERVER_NAME)
+	message_channel = discord.utils.get(guild.channels, name=DAILY_MESSAGE_CHANNEL)
+	print(f"Got channel {message_channel}")
+	await message_channel.send(DAILY_MESSAGE)
+
+@bot.event
 async def on_ready():
-	await client.change_presence(status=discord.Status.offline)
+	print("Ready")
+	await bot.change_presence(status=discord.Status.offline)
 
-@client.event
+	time_array = DAILY_MESSAGE_TIME.split(":")
+
+	scheduler = AsyncIOScheduler()
+	scheduler.add_job(func, CronTrigger(hour=time_array[0], minute=time_array[1], second=time_array[2])) 
+	scheduler.start()
+
+@bot.event
 async def on_voice_state_update(member, before, after):
 	if (after.channel != None) and (after.channel.name == KICK_CHANNEL):
 		print("Change detected on kick channel...")
 
-		guild = discord.utils.get(client.guilds, name=SERVER_NAME)
+		guild = discord.utils.get(bot.guilds, name=SERVER_NAME)
 		channel = discord.utils.get(guild.channels, name=MESSAGE_CHANNEL)
 		
 		if (not member.guild_permissions.administrator) and (not member.guild_permissions.ban_members):
@@ -36,4 +56,4 @@ async def on_voice_state_update(member, before, after):
 
 			print("Member kicked...")
 
-client.run(TOKEN)
+bot.run(TOKEN)
